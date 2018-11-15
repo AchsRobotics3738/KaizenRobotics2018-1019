78c4a242e5792949a09375549486befe9b1bb6b6
AchsRobotics3738 <45082044+AchsRobotics3738@users.noreply.github.com>
2018-11-15 16:54:32 -0600

    Update KaizenRobot.Java for Tracks

--- KaizenRobot.java
+++ KaizenRobot.java
@@ -9,35 +9,29 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class KaizenRobot
 {
-  private DcMotor FR;
-  private DcMotor FL;
-  private DcMotor BR;
-  private DcMotor BL;
-  private DcMotor Mount;
+  private DcMotor right;
+  private DcMotor left;
+  private DcMotor mount;
   
-  private Servo Claw;
+  private Servo claw;
   
   public KaizenRobot(HardwareMap hardwareMap)
   {
      // Initialize the DCMotors
-    FR  = hardwareMap.get(DcMotor.class, "Front Right");
-    BR  = hardwareMap.get(DcMotor.class, "Back Right");
-    FL  = hardwareMap.get(DcMotor.class, "Front Left");
-    BL  = hardwareMap.get(DcMotor.class, "Back Left");
-    Mount = hardwareMap.get(DcMotor.class, "Mount");
+    right  = hardwareMap.get(DcMotor.class, "Right");
+    left  = hardwareMap.get(DcMotor.class, "Left");
+    mount = hardwareMap.get(DcMotor.class, "Mount");
     
     //Initalize the Servo
-    Claw = hardwareMap.get(Servo.class, "Claw");
+    claw = hardwareMap.get(Servo.class, "Claw");
       
     // Set Default Positions of motors
-    FR.setDirection(DcMotor.Direction.FORWARD);
-    FL.setDirection(DcMotor.Direction.REVERSE);
-    BR.setDirection(DcMotor.Direction.FORWARD);
-    BL.setDirection(DcMotor.Direction.REVERSE);
-    Mount.setDirection(DcMotor.Direction.FORWARD);
+    right.setDirection(DcMotor.Direction.FORWARD);
+    left.setDirection(DcMotor.Direction.REVERSE);
+    mount.setDirection(DcMotor.Direction.FORWARD);
     
     // Set Default Positions of the servo
-    Claw.setPosition(.5);
+    claw.setPosition(.5);
   }
   
   /*Set the right wheels to an input velocity from a joystick
@@ -46,21 +40,19 @@ public class KaizenRobot
   */
   public void setRightVelocity(double velocity)
   {
-    FR.setPower(velocity);
-    BR.setPower(velocity);
+    right.setPower(velocity);
   }
   
   //Set the left wheels to an input velocity from a joystick
   public void setLeftVelocity(double velocity)
   {
-    FL.setPower(velocity);
-    BL.setPower(velocity);
+    left.setPower(velocity);
   }
   
   //Set the mount velocity to an input velocity
   public void setMountVelocity(double velocity)
   {
-    Mount.setPower(velocity);
+    mount.setPower(velocity);
   }
   
   /* Set the claw position based on a double position
@@ -70,6 +62,6 @@ public class KaizenRobot
   */
   public void setClawPosition(double position)
   {
-    Claw.setPosition(position);
+    claw.setPosition(position);
   }
 }
