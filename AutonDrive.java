c1996b05420620d161163eff107abfe8fa4db58b
Kenpoman <32337917+Kenpoman@users.noreply.github.com>
2018-11-29 15:58:54 -0600

    Create AutonDrive.java

---
+++ AutonDrive.java
@@ -0,0 +1,16 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.robot.Robot;
+import java.util.Set;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+/*
+ * Robot drive class, this is the class that actually operates the teleOP portion of the code.
+ */
+@TeleOp(name="Robot Drive", group="Iterative Opmode")
