bb42f83d85090f0f1f73d512c88c0f2b43a7e025
Kenpoman <32337917+Kenpoman@users.noreply.github.com>
2018-11-30 01:47:18 -0600

    Update AutonDrive.java

--- AutonDrive.java
+++ AutonDrive.java
@@ -10,7 +10,3 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
-/*
- * Robot drive class, this is the class that actually operates the teleOP portion of the code.
- */
-@TeleOp(name="Robot Drive", group="Iterative Opmode")
